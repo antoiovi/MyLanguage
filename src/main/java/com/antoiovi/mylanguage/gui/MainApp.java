df484909c9cf3a1f7286c1d6e57ebb5e6cce7ccd
antoiovi <antoiovi@antoiovi.com>
2019-11-10 18:35:31 +0100

    MainApp :aggiunto riferimento a licenza

--- src/main/java/com/antoiovi/mylanguage/gui/MainApp.java
+++ src/main/java/com/antoiovi/mylanguage/gui/MainApp.java
@@ -6,7 +6,9 @@ import com.antoiovi.arrangesentence.JDArrangeSentence;
  * Applicazione principale :
  * Versione 1 : Lancia l'applicazione JDOrdersentence
  * @author antoiovi
- *
+ *This work is licensed under the Creative Commons Attribution-ShareAlike 3.0 Unported License. 
+ *To view a copy of this license, visit http://creativecommons.org/licenses/by-sa/3.0/ 
+ *or send a letter to Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
  */
 public class MainApp {
 
